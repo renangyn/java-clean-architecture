3b01a40c4d950ea3e4b8b217afe7b71f49d4c7bb
Renan <renanlinhares@bb.com.br>
2025-08-05 11:42:12 -0300

    finalizando parte 3

--- src/main/java/br/com/alura/codechella/infra/gateways/RepositorioDeUsuarioJpa.java
+++ src/main/java/br/com/alura/codechella/infra/gateways/RepositorioDeUsuarioJpa.java
@@ -4,25 +4,31 @@ import java.util.List;
 
 import br.com.alura.codechella.application.gateways.RepositorioDeUsuario;
 import br.com.alura.codechella.domain.entities.usuario.Usuario;
+import br.com.alura.codechella.infra.persistence.UsuarioEntity;
 import br.com.alura.codechella.infra.persistence.UsuarioRepository;
 
 public class RepositorioDeUsuarioJpa implements RepositorioDeUsuario{
 
     private final UsuarioRepository repositorio;
+    private final UsuarioEntityMapper mapper;
 
-    public RepositorioDeUsuarioJpa(UsuarioRepository repositorio) {
+    public RepositorioDeUsuarioJpa(UsuarioRepository repositorio, UsuarioEntityMapper mapper) {
         this.repositorio = repositorio;
+        this.mapper = mapper;
     }
 
     @Override
     public Usuario cadastrarUsuario(Usuario usuario) {
-        return repositorio.save(usuario);
+        UsuarioEntity entity =  mapper.toEntity(usuario);
+        repositorio.save(entity);
+        return mapper.toDomain(entity);
     }
 
     @Override
     public List<Usuario> listarTodos() {
         // TODO Auto-generated method stub
-        return repositorio.findAll();
+        //return repositorio.findAll();
+        return null;
     }
  
 }
---
+++ src/main/java/br/com/alura/codechella/infra/gateways/UsuarioEntityMapper.java
@@ -0,0 +1,18 @@
+package br.com.alura.codechella.infra.gateways;
+
+import javax.swing.text.html.parser.Entity;
+
+import br.com.alura.codechella.domain.entities.usuario.Usuario;
+import br.com.alura.codechella.infra.persistence.UsuarioEntity;
+
+public class UsuarioEntityMapper {
+    public UsuarioEntity toEntity(Usuario usuario) {
+        return new UsuarioEntity(usuario.getCpf(), usuario.getNome(), usuario.getNascimento(), usuario.getEmail());
+
+    }
+
+    public Usuario toDomain(UsuarioEntity entity) {
+        return new Usuario(entity.getCpf(), entity.getNome(), entity.getNascimento(), entity.getEmail());
+
+    }
+}
